36acd3c81575d82b419d027d2a5acddb15434e39
Rishabh Verma <10rishabhv@gmail.com>
2023-04-05 19:49:14 +0530

    fix recipe id page (temporary)

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -1,11 +1,11 @@
 'use client'
+
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
 const id = "r";
 
 const Post : React.FC = () => {
   const router = useRouter()
-  const { id } = useSearchParams();
 
   return <p>Post: {id}</p>
 }
@@ -14,8 +14,5 @@ const Post : React.FC = () => {
 export async function getStaticPaths() {
     // Return a list of possible value for id
   }
-  
-  export async function getStaticProps({ params }) {
-    // Fetch necessary data for the blog post using params.id
-  }
+
 export default Post
