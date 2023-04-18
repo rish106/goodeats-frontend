7943fc98e5e7fd3b750e911e1a66ed0aed26d169
Dhruv <Dhruvahlawat573@gmail.com>
2023-04-18 19:51:05 +0530

    the home page looks much better now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import { Metadata } from 'next'
 import LargeHeading from '@/components/ui/LargeHeading'
 // import { scrollArea } from '@/components/RecipeSlide'
+import Image from 'next/image'
 import {RecipeBox}from '@/components/RecipeSlide'
 import { feedRecipes } from '@/public/data'
 import Paragraph from '@/components/ui/Paragraph'
@@ -17,15 +18,18 @@ export default function Home() {
   return (
     <div className='relative h-screen flex items-center justify-center overflow-x-hidden bg-slate-200 overflow-y-scroll'>
       <div className='container pt-32 max-w-full w-full h-full mx-2 md:mx-5'>
-        <div className='h-full gap-6 flex flex-col justify-start lg:justify-center items-center bg-slate-800 lg:items-start'>
-        <div className='relative gap-4 flex flex-col h-screen overflow-x-hidden items-center justify-center bg-slate-400'>
-          <LargeHeading> Looking for a snack? </LargeHeading>  
-          <LargeHeading className='text-wrap' size='xs'> Whether you want easy to make homemade food <br></br>or 
-          exquisite dishes and delicacies,<br></br> We've got you covered.</LargeHeading>  
-          <Paragraph className=''>
-           welcome to GoodEats! </Paragraph>     
+        <div className='h-full w-full gap-6 grid grid-row-flow grid-cols-2 justify-start lg:justify-center items-center  lg:items-start'>
+        <div className='relative w-full gap-10 flex flex-col h-[530px] items-center justify-center '>
+          <LargeHeading className='font-semibold'> Looking for a snack? </LargeHeading>  
+          <LargeHeading className='text-wrap font-semibold' size='sm'> Whether you want easy to make homemade food or 
+          exquisite dishes and delicacies, We've got you covered.</LargeHeading>  
+          <LargeHeading className='font-semibold'>
+           welcome to GoodEats! </LargeHeading>     
         </div>
+          <div className='w-full h-[600px]'>
+          <Image className='w-full h-600 object-cover' src='fryingpantop.jpg' alt='frying pan' width={1000} height={1000} />
           </div>
+        </div>
          <div className='items-center  justify-center flex flex-col gap-10 h-[900px] bg-slate-100'>
           <LargeHeading className='font-sans font-medium'>Top Rated</LargeHeading>
           <div className=' flex-row h-[600px] justify-start items-center inset-x-20 grid grid-rows-1 grid-flow-col gap-8 overflow-y-visible overflow-x-scroll 2xl:max-w-screen-2xl xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md sm:max-w-screen-sm'>
