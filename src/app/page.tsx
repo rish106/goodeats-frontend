91388a0111567b024e95e8b3a146d42066425b9b
Dhruv <Dhruvahlawat573@gmail.com>
2023-04-18 18:04:45 +0530

    added RecipeSlide and updated home page, also added ratings in RecipeCard and exported the type as well

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { Inter } from 'next/font/google'
 import { Metadata } from 'next'
 import LargeHeading from '@/components/ui/LargeHeading'
+// import { scrollArea } from '@/components/RecipeSlide'
+import {RecipeBox}from '@/components/RecipeSlide'
+import { feedRecipes } from '@/public/data'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,14 +14,39 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className='relative h-screen flex items-center justify-center overflow-x-hidden'>
+    <div className='relative h-screen flex items-center justify-center overflow-x-hidden overflow-y-scroll'>
       <div className='container pt-32 max-w-full w-full h-full mx-2 md:mx-5'>
         <div className='h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start'>
-          <LargeHeading size='lg' className='text-black'>
-            Get some nice recipes
-          </LargeHeading>
+        <div className='relative flex flex-col h-screen overflow-x-hidden items-center justify-center '>
+          <LargeHeading> Ready to eat your heart out?</LargeHeading>         
         </div>
-      </div>
-    </div>
+          </div>
+         <div className='items-center justify-center flex flex-col h-[440px]'>
+          <div className='flex-none justify-center inset-x-12 grid grid-rows-1 grid-flow-col gap-4 overflow-x-scroll 2xl:max-w-screen-2xl xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md sm:max-w-screen-sm'>
+                <RecipeBox recipeName= {feedRecipes[0].name} recipeAuthor={feedRecipes[0].username} recipeId={feedRecipes[0].recipeId} 
+                recipeImage={feedRecipes[0].imageUrl} recipeDescription={feedRecipes[0].description} recipeRating={feedRecipes[0].rating}/>
+                <RecipeBox recipeName= {feedRecipes[0].name} recipeAuthor={feedRecipes[0].username} recipeId={feedRecipes[0].recipeId} 
+                recipeImage={feedRecipes[1].imageUrl} recipeDescription={feedRecipes[0].description} recipeRating={feedRecipes[0].rating}/>
+                <RecipeBox recipeName= {feedRecipes[0].name} recipeAuthor={feedRecipes[0].username} recipeId={feedRecipes[0].recipeId} 
+                recipeImage={feedRecipes[1].imageUrl} recipeDescription={feedRecipes[0].description} recipeRating={feedRecipes[0].rating}/>
+                <RecipeBox recipeName= {feedRecipes[0].name} recipeAuthor={feedRecipes[0].username} recipeId={feedRecipes[0].recipeId} 
+                recipeImage={feedRecipes[1].imageUrl} recipeDescription={feedRecipes[0].description} recipeRating={feedRecipes[0].rating}/>
+                <RecipeBox recipeName= {feedRecipes[0].name} recipeAuthor={feedRecipes[0].username} recipeId={feedRecipes[0].recipeId} 
+                recipeImage={feedRecipes[1].imageUrl} recipeDescription={feedRecipes[0].description} recipeRating={feedRecipes[0].rating}/>
+                <RecipeBox recipeName= {feedRecipes[0].name} recipeAuthor={feedRecipes[0].username} recipeId={feedRecipes[0].recipeId} 
+                recipeImage={feedRecipes[1].imageUrl} recipeDescription={feedRecipes[0].description} recipeRating={feedRecipes[0].rating}/>
+                <RecipeBox recipeName= {feedRecipes[0].name} recipeAuthor={feedRecipes[0].username} recipeId={feedRecipes[0].recipeId} 
+                recipeImage={feedRecipes[1].imageUrl} recipeDescription={feedRecipes[0].description} recipeRating={feedRecipes[0].rating}/>
+                <RecipeBox recipeName= {feedRecipes[0].name} recipeAuthor={feedRecipes[0].username} recipeId={feedRecipes[0].recipeId} 
+                recipeImage={feedRecipes[1].imageUrl} recipeDescription={feedRecipes[0].description} recipeRating={feedRecipes[0].rating}/>
+                <RecipeBox recipeName= {feedRecipes[0].name} recipeAuthor={feedRecipes[0].username} recipeId={feedRecipes[0].recipeId} 
+                recipeImage={feedRecipes[1].imageUrl} recipeDescription={feedRecipes[0].description} recipeRating={feedRecipes[0].rating}/>
+            </div>
+          </div>
+
+
+         </div>
+       </div>
   )
+ 
 }
--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -4,15 +4,16 @@ import Image from 'next/image'
 import LargeHeading from '@/ui/LargeHeading'
 import Paragraph from '@/ui/Paragraph'
 
-interface RecipeCardProps {
+export interface RecipeCardProps {
   recipeName: string
   recipeDescription: string
   recipeAuthor: string
   recipeId: number
   recipeImage: string
+  recipeRating: number
 }
 
-export function RecipeCard({recipeName, recipeAuthor, recipeImage, recipeDescription, recipeId}: RecipeCardProps) {
+export function RecipeCard({recipeName, recipeAuthor, recipeImage, recipeDescription, recipeId, recipeRating}: RecipeCardProps) {
   return (
     <div key={recipeId} className='flex flex-col md:flex-row gap-4 w-full max-w-7xl'>
       <Image src={`/static/recipe_pics/${recipeImage}`} alt={recipeName} width={200} height={200} className='rounded-lg' />
