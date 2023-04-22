6e15d33b60a69ec883f5cdffbfe4d2d3f9567fb3
Dhruv <Dhruvahlawat573@gmail.com>
2023-04-22 23:54:40 +0530

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,12 @@ export const metadata: Metadata = {
   title: 'Goodeats',
   description: 'What would you like to cook today',
 }
+async function getAllRecipes() {
+  const res = await fetch('http://127.0.0.1:5000/');
+  return res.json();
+}
+export default async function Home() {
+  // let feedRecipes = await getAllRecipes();
 
 
 async function getTopRecipes() {
@@ -27,6 +33,7 @@ export default async function Home() {
   const [topRecipes] = await Promise.all([topRecipesPromise]);
 
   return (
+    
     <div className='relative h-screen flex items-center justify-center overflow-x-hidden bg-slate-200 overflow-y-scroll'>
       <div className='container pt-32 max-w-full w-full h-full mx-2 md:mx-5'>
         <div className='h-full w-full gap-6 grid grid-row-flow grid-cols-2 justify-start lg:justify-center items-center  lg:items-start'>
