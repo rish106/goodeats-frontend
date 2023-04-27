8a2a14f6203dd423e3263d1e816b74e641743a07
Rishabh Verma <10rishabhv@gmail.com>
2023-04-28 01:00:01 +0530

    fix hosts, post recipe

--- next.config.js
+++ next.config.js
@@ -8,19 +8,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'w7.pngwing.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'img.sndimg.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.themealdb.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        hostname: '*',
       }
     ],
   },
